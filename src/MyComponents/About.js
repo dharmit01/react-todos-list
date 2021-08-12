af6a60a9498e3c5e2558f7c380636db85c4348cc
Dharmit Shah <81405914+dharmit01@users.noreply.github.com>
2021-08-12 14:31:41 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className="container">
+            <p>Hello My Name is Dharmit</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam vero eaque blanditiis. Aspernatur laborum ea praesentium alias harum iste enim a inventore amet omnis, beatae exercitationem vel eum repellendus porro nisi. Culpa, laboriosam incidunt?</p>
+        </div>
+    )
+}
